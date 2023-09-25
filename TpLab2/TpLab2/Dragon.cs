065949a6497f8d0f7486cbcad28c7abb562cadda
MicaCaceres <126696822+MicaCaceres@users.noreply.github.com>
2023-09-25 12:28:00 -0300

    Clases
    Escribi las clases aunque no implemente mucho los metodos(por no decir casi nada, dsp si puedo le agrego mas cosas)

---
+++ TpLab2/TpLab2/Dragon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpLab2
+{
+    internal class Dragon:Piezas
+    {
+        public Dragon(int posX, int posY) : base(posX, posY) 
+        {
+        }
+        public override void MoverPieza(int x, int y) 
+        {
+        
+        }
+    }
+}
---
+++ TpLab2/TpLab2/Experto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpLab2
+{
+    internal class Experto:Intermedio
+    {
+        public Calabozo[] calabozo = new Calabozo[4];
+        public void CrearCalabozos() 
+        {
+            for (int i = 0; i < 4; i++) 
+            {
+                Calabozo calabozonuevo;
+               int posicion= new calabozonuevo.GenerarCalabozos();
+            
+            }
+
+        }
+    }
+}
