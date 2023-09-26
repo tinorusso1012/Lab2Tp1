817f52ba2b16326535d3f4113e7397b1a7d12344
tinorusso1012 <valentinorusso2002@gmail.com>
2023-09-26 15:44:24 -0300

    Cambios Generales

---
+++ TpLab2/Calabozo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpLab2
+{
+    internal class Calabozo:Piezas
+    {
+        public Calabozo(int posX, int posY) : base(posX, posY) 
+        {
+        }
+        public int GenerarCalabozos() 
+        {
+            Random random = new Random();
+            int posicion = random.Next(0, 49);
+            return posicion;
+        }
+        public override void MoverPieza(int x, int y)
+        {
+            base.MoverPieza(x, y);
+        }
+    }
+}
---
+++ TpLab2/Jugador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpLab2
+{
+    internal class Jugador
+    {
+        private string nombre;
+        private int posicion;
+        public int Posicion;
+        private bool turno = true;
+        public string Nombre { get; private set; }
+        public int CantidadGanadas { get; set; }
+        public Jugador() 
+        {
+            
+        }
+        public bool VerificarTurno() 
+        {
+            if (turno != false)
+            {
+
+            }
+            return turno;
+        }
+        public void Jugar() 
+        {
+    
+        
+        }
+    }
+}
